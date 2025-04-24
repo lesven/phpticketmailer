9f0a73b14b9558a40496b1ec4c547d0af6e19680
Sven Heising <sven.heising@gmail.com>
2025-04-24 10:21:27 +0200

    Remove auto-generated SQL for smtpconfig table creation and deletion in migration

--- migrations/Version20250424063754.php
+++ migrations/Version20250424063754.php
@@ -19,17 +19,11 @@ final class Version20250424063754 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            CREATE TABLE smtpconfig (id INT AUTO_INCREMENT NOT NULL, host VARCHAR(255) NOT NULL, port INT NOT NULL, username VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT NULL, use_tls TINYINT(1) NOT NULL, sender_email VARCHAR(255) NOT NULL, sender_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
-        SQL);
+       
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            DROP TABLE smtpconfig
-        SQL);
+       
     }
 }
