943000c927aa840ff10817977cf160dc80226460
Sven Heising <sven.heising@gmail.com>
2025-09-09 09:43:20 +0200

    Füge EmailSkippedEvent hinzu und dispatch bei E-Mail-Überspringen-Szenarien

---
+++ src/Event/Email/EmailSkippedEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Event\Email;
+
+use App\Event\AbstractDomainEvent;
+use App\ValueObject\TicketId;
+use App\ValueObject\Username;
+use App\ValueObject\EmailAddress;
+use App\ValueObject\EmailStatus;
+
+/**
+ * Event: Eine E-Mail wurde übersprungen
+ * 
+ * Wird ausgelöst, wenn eine Ticket-E-Mail nicht versendet wurde, weil:
+ * - Das Ticket bereits verarbeitet wurde
+ * - Es sich um ein Duplikat in der CSV handelt
+ * - Der Benutzer von Umfragen ausgeschlossen ist
+ * - Kein Benutzer/E-Mail gefunden wurde
+ * 
+ * Ermöglicht Audit-Logging, Statistiken und Follow-up-Aktionen.
+ */
+class EmailSkippedEvent extends AbstractDomainEvent
+{
+    public function __construct(
+        public readonly TicketId $ticketId,
+        public readonly Username $username,
+        public readonly ?EmailAddress $email,
+        public readonly EmailStatus $status,
+        public readonly bool $testMode = false,
+        public readonly ?string $ticketName = null
+    ) {
+        parent::__construct();
+    }
+}
